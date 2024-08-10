3a4777fb3d26b098a953ba9deba848cf75ebd582
Ali Gorgani <52858828+Ali-Gorgani@users.noreply.github.com>
2024-08-10 17:52:59 +0000

    set changes for portfolio site

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Ali Gorgani",
+  title: "Backend Developer",
+  email: "ali.gorgani.fj@gmail.com",
+  gitHub: "Ali-Gorgani",
+  instagram: "ali_gorgani_fj",
+  linkedIn: "ali-gorgani-firoozjaee",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "",
+  youTube: "",
 };
 
 const primaryColor = "#4E567E";
